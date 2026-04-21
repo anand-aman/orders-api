9bfb99d68437894a1ff465d5b86a9921cab81fb3
anand-aman <itsamananand@gmail.com>
2026-04-21 21:53:26 +0530

    repo second

--- application/routes.go
+++ application/routes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 
-	"github.com/anand-aman/orders-api/application/handler"
+	"github.com/anand-aman/orders-api/handler"
 )
 
 func loadRoutes() *chi.Mux {
