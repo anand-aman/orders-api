d57154f6be89af63eeb9df1a5e4f7f7d435c0caf
anand-aman <itsamananand@gmail.com>
2026-04-22 20:44:41 +0530

    cors added

--- application/routes.go
+++ application/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 
 	"github.com/anand-aman/orders-api/handler"
 )
@@ -18,6 +19,12 @@ func (a *App) loadRoutes() {
 	})
 
 	router.Route("/orders", a.loadOrderRoutes)
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Content-Type"},
+	}))
 	a.router = router
 }
 
